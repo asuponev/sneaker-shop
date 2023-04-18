0835de0303979615619a600c282f142941f748af
Andrew <andrew.suponev@gmail.com>
2023-04-18 13:03:16 +0400

    feat: implement sneaker cards view

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,17 @@
+import styles from './Container.module.scss'
+
+interface IContainerProps {
+  children: React.ReactNode
+}
+
+const Container: React.FC<IContainerProps> = ({
+  children
+}) => {
+  return (
+    <div className={styles.container}>
+      {children}
+    </div>
+  )
+}
+
+export default Container
--- src/components/sneaker-cards/SneakerCards.tsx
+++ src/components/sneaker-cards/SneakerCards.tsx
@@ -1,16 +1,21 @@
 import { ISneakerData } from '../interfaces/sneaker.interface'
 
+import Container from '../container/Container'
 import SneakerCard from '../sneaker-card/SneakerCard'
+
+import styles from './SneakerCards.module.scss'
 
 const SneakerCards: React.FC<ISneakerData> = ({
   sneakers
 }) => {
   return (
-    <>
-      {sneakers.length ?
-        sneakers.map((sneaker) => <SneakerCard key={sneaker.id} sneaker={sneaker} />)
-        : <div>Sneakers not found</div>}
-    </>
+    <Container>
+      <div className={styles.cards}>
+        {sneakers.length ?
+          sneakers.map((sneaker) => <SneakerCard key={sneaker.id} sneaker={sneaker} />)
+          : <div>Sneakers not found</div>}
+      </div>
+    </Container>
   )
 }
 
