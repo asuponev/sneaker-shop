d6f517212c1d5df4bb00bd518f67c76cc1da3982
Andrew <andrew.suponev@gmail.com>
2023-04-19 00:24:52 +0400

    refactor: insert client component into server component for main page (delete screens component)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
 import { GetServerSideProps, NextPage } from 'next'
-import Head from 'next/head'
 
 import { SneakerService } from '@/services/sneaker.service'
 import { ISneakerData } from '@/interfaces/sneaker.interface'
 
 import Layout from '@/components/layout/Layout'
-import MainScreen from '@/components/screens/main/MainScreen'
+import SneakerCards from '@/components/sneaker-cards/SneakerCards'
+import Basket from '@/components/basket/Basket'
 
 const Home: NextPage<ISneakerData> = ({
   sneakers
 }) => {
   return (
     <Layout>
-      <MainScreen sneakers={sneakers} />
+      <div className='screen'>
+        <SneakerCards sneakers={sneakers} />
+        <Basket />
+      </div>
     </Layout>
   )
 }
