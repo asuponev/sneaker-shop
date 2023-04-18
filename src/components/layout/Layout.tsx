3fdd7a6396a48fedbcaa9bdac9079d568f69d51b
Andrew <andrew.suponev@gmail.com>
2023-04-18 22:15:27 +0400

    feat: implement layout

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren } from 'react'
+import Head from 'next/head'
+
+import Header from '@/components/header/Header'
+
+interface ILayoutProps {
+  title?: string
+  description?: string
+}
+
+const Layout: React.FC<PropsWithChildren<ILayoutProps>> = ({
+  children,
+  title = 'SB.shop',
+  description = 'Sneaker Shop'
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta name='og:title' content={title} />
+        <meta name='og:description' content={description} />
+      </Head>
+      <Header />
+      <main>
+        {children}
+      </main>
+    </>
+  )
+}
+
+export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,16 @@ import Head from 'next/head'
 import { SneakerService } from '@/services/sneaker.service'
 import { ISneakerData } from '@/interfaces/sneaker.interface'
 
-import Header from '@/components/header/Header'
+import Layout from '@/components/layout/Layout'
 import MainScreen from '@/components/screens/main/MainScreen'
 
 const Home: NextPage<ISneakerData> = ({
   sneakers
 }) => {
   return (
-    <>
-      <Head>
-        <title>SB.shop</title>
-        <meta name="description" content="Sneaker shop" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <main>
-        <MainScreen sneakers={sneakers} />
-      </main>
-    </>
+    <Layout>
+      <MainScreen sneakers={sneakers} />
+    </Layout>
   )
 }
 
